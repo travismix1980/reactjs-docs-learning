f3aa2b4dc638759910dcf5fe4e78cf932a28cbea
Travis Mix <travismix1980@gmail.com>
2020-10-17 18:07:24 -0700

    Finished Reactjs guide sections 1 (Hello World) and 2 (Introducing JSX)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const formatName = (user) => {
+    return `${user.firstName} ${user.lastName}`;
+}
+
+const getGreeting = (user) => {
+    if(user){
+        return <h1>Hello, {formatName(user)}</h1>
+    }
+    return <h1>Hello, stranger.</h1>
+}
+
+const user = {
+    firstName: 'Travis',
+    lastName: 'Mix',
+    avatarUrl: 'https://www.w3schools.com/howto/img_avatar.png',
+}
+
+const element = (
+    <div tabIndex="0">
+        {getGreeting(user)}
+        <img src={user.avatarUrl} alt="avatar"/>
+    </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
+
