8339e191acf6c71c9d7b3beeff9d896c48716b52
Travis Mix <travismix1980@gmail.com>
2020-10-17 20:17:02 -0700

    Finished Reactjs main concepts section 3 (Components and Props) and section 4 (State and Lifecycle)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Clock from "./components/Clock";
+
+function App(){
+    return (
+        <div>
+            <Clock />
+            <Clock />
+            <Clock />
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./App";
 
-const tick = () =>{
-    const element = (
-        <div>
-            <h1>Hello World</h1>
-            <h2>It is {new Date().toLocaleTimeString()}</h2>
-        </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
-}
 
-setInterval(tick, 1000);
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
