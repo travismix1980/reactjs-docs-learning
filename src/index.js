7d454fac1b298b19f3da29ea7eb0f422b48f156e
Travis Mix <travismix1980@gmail.com>
2020-10-17 19:33:00 -0700

    Finished Reactjs main concepts section 3 (Rendering Elements)

--- src/index.js
+++ src/index.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const formatName = (user) => {
-    return `${user.firstName} ${user.lastName}`;
+const tick = () =>{
+    const element = (
+        <div>
+            <h1>Hello World</h1>
+            <h2>It is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
 }
 
-const getGreeting = (user) => {
-    if(user){
-        return <h1>Hello, {formatName(user)}</h1>
-    }
-    return <h1>Hello, stranger.</h1>
-}
-
-const user = {
-    firstName: 'Travis',
-    lastName: 'Mix',
-    avatarUrl: 'https://www.w3schools.com/howto/img_avatar.png',
-}
-
-const element = (
-    <div tabIndex="0">
-        {getGreeting(user)}
-        <img src={user.avatarUrl} alt="avatar"/>
-    </div>
-);
-
-
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
-
+setInterval(tick, 1000);
