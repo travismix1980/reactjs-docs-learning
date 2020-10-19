b5113980967b20813c8038e6740d2a231b0d7284
Travis Mix <travismix1980@gmail.com>
2020-10-18 23:47:16 -0700

    removed the bullet points from the UL and added a little bit of styling for fun idk

--- src/components/NumberList.js
+++ src/components/NumberList.js
@@ -3,14 +3,27 @@ import React from "react";
 function NumberList(props){
     const numbers = props.numbers;
     const listItems = numbers.map(number =>(
-        <li key={number.toString()}>
+        <li key={number.toString()} style={liStyle}>
             {number}
         </li>
     ));
 
     return(
-        <ul>{listItems}</ul>
+        <ul style={listStyle}>{listItems}</ul>
     );
 }
 
+const listStyle = {
+    listStyleType: "none",
+    margin: "0",
+    padding: "0",
+}
+
+const liStyle = {
+    cursor: "pointer",
+    fontWeight: "bold",
+    fontSize: "24px",
+    lineHeight: "1.5",
+    color: "#333",
+}
 export default NumberList;
